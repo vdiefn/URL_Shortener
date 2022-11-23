1e9e3deb231c7f38b2a7821b87eae6f46cb8fd0a
vdiefn <ddhkxxhxx@hotmail.com>
2022-11-23 14:07:24 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+const port = 3000
+
+app.engine('handlebars', exphbs({defaultLayout : 'main'}))
+app.set('view engine', 'handlebars')
+
+app.use(express.urlencoded({ extended: true}))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log( `Express is  running on http://localhost:${port}`)
+})
