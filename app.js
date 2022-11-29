365db72cf5755cec7273140e2b02a42b20af7fec
vdiefn <ddhkxxhxx@hotmail.com>
2022-11-29 17:06:59 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const urlShortener = require('./urlShortener')
 const Data = require('./models/data')
+const routes = require('./routes')
 
 
 if (process.env.NODE_ENV !== 'production') {
@@ -28,54 +29,13 @@ app.engine('handlebars', exphbs({defaultLayout : 'main'}))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.urlencoded({ extended: true}))
+app.use(routes)
+
 
 app.get('/', (req, res) => {
   res.render('index')
 })
 
-//確認db中是否已有該網址的資訊
-app.post('/', (req, res) => {
-  let originalURL = req.body.url
-  
-  console.log('req.body:', originalURL)
-  
-  return Data.findOne({ 'originalURL': originalURL })
-    .then((data)=>{
-      //該筆資料已存在db中
-      if (data !== null) {
-        console.log('already had!')
-        console.log('find:', data)
-        //將原有資料叫出來
-        let shortenURL = data.shortenURL
-        return res.render('index', { shortenURL: shortenURL, originalURL: originalURL })
-      //該筆資料不存在db中
-      } else {
-        console.log('add to db!')
-        //新增該筆資料
-        let shortenURL = urlShortener()
-        return Data.create({ originalURL, shortenURL })
-        .then(() => res.render('index', { shortenURL: shortenURL, originalURL: originalURL }))
-      }
-      })
-    })
-    
-//使用產生的短網址連至指定網站
-app.get('/:shortenURL', (req, res) => {
-  let shortURL = req.params.shortenURL
-  console.log('shortURL:', shortURL)
-  return Data.findOne({ 'shortenURL': `https://localhost:3000/${shortURL}`})
-    .then((data) => {
-      if (data !== null) {
-        console.log('find the data:', data)
-        console.log('originalURL:', data.originalURL)
-        res.redirect(data.originalURL)
-      } else {
-        console.log('error')
-        res.render('error', { shortURL })
-      }
-     
-    })
-})
 
 app.listen(port, () => {
   console.log( `Express is running on http://localhost:${port}`)
